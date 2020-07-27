27ca2988db19e7a736456ba9b36a171042b60cfc
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2020-07-27 02:21:33 +0100

    Example atom.

---
+++ src/components/ExampleInput.component.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { textState } from "../atoms/todo.atom";
+
+const ExampleInput = () => {
+    const [text, setText] = useRecoilState(textState);
+
+    const onChange = (event: any) => {
+      setText(event.target.value);
+    };
+
+   return ( <div className="">
+        <input type="text" value={text} onChange={onChange} />
+        <p>
+        Echo: {text}
+        </p>
+     </div>)
+}
+
+export default ExampleInput;
