258ba36362646071079c5845b37a15843c3fc387
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2020-08-01 19:59:59 +0100

    Added Css class.

--- src/components/ExampleInput.component.tsx
+++ src/components/ExampleInput.component.tsx
@@ -9,7 +9,7 @@ const ExampleInput = () => {
       setText(event.target.value);
     };
 
-   return ( <div className="">
+   return ( <div className="Echo-container">
         <input type="text" value={text} onChange={onChange} />
         <p>
         Echo: {text}
